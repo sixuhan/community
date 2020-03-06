084f932093f140ff40b60502f903ef2806e7edd2
zhangtianxu <a823525514@gmail.com>
2020-03-06 09:49:53 +0800

    使用拦截器来实现账号设置的功能

--- src/main/java/com/nowcoder/community/controller/LogingController.java
+++ src/main/java/com/nowcoder/community/controller/LogingController.java
@@ -5,17 +5,21 @@ import com.google.code.kaptcha.Producer;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityConstant;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.awt.image.BufferedImage;
@@ -26,6 +30,9 @@ import java.util.Map;
 @Controller
 public class LogingController implements CommunityConstant {
     private static final Logger logger = LoggerFactory.getLogger(LogingController.class);
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
 
     @Autowired
     private UserService userService;
@@ -98,5 +105,40 @@ public class LogingController implements CommunityConstant {
             //流由spring来进行管理
             logger.error("验证码响应失败" + e.getMessage());
         }
+    }
+
+    @RequestMapping(path = "/login",method = RequestMethod.POST)
+    public String login(String username,String password,String code,boolean rememberme, Model model,HttpSession session, HttpServletResponse response){
+
+        //检查验证码
+        String kaptcha = (String)session.getAttribute("kaptcha");
+        if (StringUtils.isBlank(kaptcha) || StringUtils.isBlank(code) || !kaptcha.equalsIgnoreCase(code)){
+            //kaptcha.equalsIgnoreCase() 该方法忽略大小写
+            model.addAttribute("codeMsg","验证码不正确");
+            return "/site/login";
+        }
+
+        //检查账号密码
+        int expiredSeconds = rememberme ? REMEMBER_EXPIRED_SECONDS : DEFAULT_EXPIRED_SECONDS;
+        Map<String, Object> map = userService.Login(username, password, expiredSeconds);
+        if(map.containsKey("ticket")){
+            Cookie cookie = new Cookie("ticket", map.get("ticket").toString());
+            cookie.setPath(contextPath);
+            cookie.setMaxAge(expiredSeconds);
+            response.addCookie(cookie);
+            return "redirect:/index";
+        }else {
+            model.addAttribute("usernameMsg",map.get("usernameMsg"));
+            model.addAttribute("passwordMsg",map.get("passwordMsg"));
+            return "/site/login";
+        }
+
+
+    }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.GET)
+    public String logout(@CookieValue("ticket") String ticket){
+        userService.logout(ticket);
+        return "redirect:/login";
     }
 }
--- src/test/java/com/nowcoder/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -1,8 +1,10 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -23,6 +26,9 @@ public class MapperTests {
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
     @Test
     public  void testSelectUser(){
@@ -42,6 +48,27 @@ public class MapperTests {
 
         int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
+    }
+
+    @Test
+    public void testInsertLoginTicket(){
+        LoginTicket ticket = new LoginTicket();
+        ticket.setUserId(101);
+        ticket.setTicket("abc");
+        ticket.setStatus(0);
+        ticket.setExpired(new Date(System.currentTimeMillis() +1000 * 60 *10));
+
+        loginTicketMapper.insertLoginTicket(ticket);
+    }
+
+    @Test
+    public void TestSelectLoginTicket(){
+        LoginTicket ticket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(ticket);
+
+        loginTicketMapper.updateStatus("abc",1);
+        LoginTicket abc = loginTicketMapper.selectByTicket("abc");
+        System.out.println(abc);
     }
 
 }
